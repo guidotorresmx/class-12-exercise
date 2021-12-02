0baaa21609af72d0e20d1552be536c5500ffec55
Marco Guido <guido-torres@hotmail.com>
2021-12-01 18:00:03 -0600

    added coockbookCli.add test

--- test/cookbook-cli.test.js
+++ test/cookbook-cli.test.js
@@ -2,9 +2,12 @@ const { Cookbook } = require('../src/cookbook');
 const { CookbookCli } = require('../src/cookbook-cli');
 
 describe('CookbookCli', () => {
+  const cookbook = new Cookbook();
   describe('Adding recipes', () => {
     test('should accept the recipe information and display the correct message', () => {
-
+      const cookbookCli = new CookbookCli(cookbook);
+      const message = cookbookCli.add('tacos', ['guisado', 'tortilla', 'salsa']);
+      expect(message).toBe(`Successfully added the following recipe: tacos`);
     });
   });
 
